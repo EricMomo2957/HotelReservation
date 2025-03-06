c029772157ebdeec5c70013a75a8b9923c1c88a0
EricMomo2957 <momoe2957@gmail.com>
2025-03-06 10:38:46 +0800

    Add files via upload
    Other Version

---
+++ Controllers/AccessController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using HotelManagement.Models;
+using System.Threading.Tasks;
+
+namespace HotelManagement.Controllers
+{
+    public class AccessController : Controller
+    {
+        public IActionResult Login()
+        {
+
+            ClaimsPrincipal claimUser = HttpContext.User;
+
+            if (claimUser.Identity.IsAuthenticated)
+                return RedirectToAction("Index", "Home");
+
+
+            return View();
+
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Login(VMLogin modelLogin)
+        {
+            if (modelLogin.Email == "admin55@yahoo.com" &&
+                modelLogin.Password == "123")
+            {
+                List<Claim> claims = new List<Claim>()
+                {
+                    new Claim(ClaimTypes.NameIdentifier, modelLogin.Email),
+                    new Claim("OtherProperties", "ExampleRole")
+                };
+
+                ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, 
+                    CookieAuthenticationDefaults.AuthenticationScheme);
+
+                AuthenticationProperties properties = new AuthenticationProperties()
+                {
+                    AllowRefresh = true,
+                };
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(claimsIdentity), properties);
+                return RedirectToAction("Index", "Home");
+            }
+            ViewData["ValidateMesaage"] = "Invalid Credential";
+            return View();
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using HotelManagement.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace HotelManagement.Controllers;
+[Authorize]
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public async Task<IActionResult> Logout()
+    {
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        return RedirectToAction("Login", "Access");
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
